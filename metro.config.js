d455c0595f0c3dd298a8dc138cc13f5fcd248256
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-09-08 20:51:24 -0400

    load small whisper model from assets

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+config.resolver.assetExts.push('bin', 'mil');
+
+module.exports = config;
--- utils/whisper.ts
+++ utils/whisper.ts
@@ -2,7 +2,7 @@ import { initWhisper } from 'whisper.rn'
 
 export async function transcribe(fileUri: string): Promise<string> {
     const whisperContext = await initWhisper({
-        filePath: 'file://.../ggml-tiny.en.bin',
+        filePath: require('../assets/ggml-tiny.en.bin'),
     })
     const options = { language: 'en' }
     const { stop, promise } = whisperContext.transcribe(fileUri, options)
